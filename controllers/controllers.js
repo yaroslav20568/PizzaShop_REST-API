22770aa836f535fcf8946897ec70ae346146adf7
Yaroslav <ravlushevich.iarek@mail.ru>
2021-09-07 21:22:36 +0300

    add filter and sort querys

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,7 +1,15 @@
 const db = require('./../config/db');
 
 const getPizzasFromDB = (req, resp) => {
-    db.query('SELECT * FROM pizzashop.pizzas', (err, results) => {
+    const category = req.query.category;
+    const _sort = req.query._sort;
+    const _order = req.query._order;
+
+    const queryStr = `
+        SELECT * FROM pizzas ${category ? `WHERE category = ${category}` : ''} ORDER BY ${_sort} ${_order}
+    `;
+
+    db.query(queryStr, (err, results) => {
         if(err) {
             console.log(err);
         } else {
